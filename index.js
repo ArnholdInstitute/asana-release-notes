fd5881ce33bfe7490083c31b6bf9507db823a3e6
Kyle <kyle.landell@gmail.com>
2018-01-11 13:39:00 -0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,42 @@
+const prompt = require('prompt');
+const csv = require('csvtojson');
+const fs = require('fs');
+
+prompt.start();
+
+prompt.get([{
+  name: 'version',
+  type: 'string',
+  description: 'Enter the version number (ex. 1.0.0)',
+  pattern: /^\d+\.\d+\.\d+$/,
+  message: 'The version must be in the format x.x.x',
+  required: true,
+}], (err, { version }) => {
+  if (err) throw err;
+
+  const items = [];
+  csv().fromFile(`./releases/v${version.replace(/\./g, '')}.csv`)
+    .on('json', ({ 'Task ID': taskId, Name: name }) => {
+      items.push({ taskId, name });
+    })
+    .on('done', (err) => {
+      if (err) throw err;
+
+      // generate the release document
+      const notes =
+      `# Version ${version} Release Notes
+      Tasks may be viewed directly on Asana via \`https://app.asana.com/0/430541393561890/{itemID}\`
+      &nbsp;
+      ##### Items completed:
+      ${items.sort(({ taskId: a }, { taskId: b }) => a > b ? 1 : (a < b ? -1 : 0))
+        .map(({ taskId, name }) => (
+          `* \`${taskId}\` - ${name}`
+        )).join('\n')}
+      `.replace(/ {2,}/g, ''); // replace groups of 2 or more spaces with an empty string for proper formatting
+
+      // write the file out to the releases directory
+      fs.writeFile(`./releases/v${version}.md`, notes, (err) => {
+        if (err) throw err;
+      })
+    });
+});
---
+++ releases/README.md
@@ -0,0 +1,2 @@
+# Releases directory
+Drop in a csv file of the Asana version export, then run the parser. The completed release notes will be saved to this directory as `{version#}.md`.
