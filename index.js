a9b24cc63e6f179fe9581f4726ea21a46e99b72d
Kyle <kyle.landell@gmail.com>
2018-01-23 16:10:35 -0500

    Use Asana api instead of CSV exports

--- index.js
+++ index.js
@@ -1,51 +1,90 @@
-const prompt = require('prompt');
-const csv = require('csvtojson');
 const chalk = require('chalk');
+const csv = require('csvtojson');
+const fetch = require('node-fetch');
 const fs = require('fs');
+const prompt = require('prompt');
 
-const ASANA_BASE_URL = 'https://app.asana.com/0/430541393561890';
-const VALIDATOR = /^\d+\.\d+\.\d+$/;
-const VALIDATION_MSG = 'The version must be in the format x.x.x';
+// load in environment variables
+require('dotenv').config()
+const { ASANA_ACCESS_TOKEN, ASANA_API_VERSION, ASANA_PROJECT_ID } = process.env;
+if (!(ASANA_ACCESS_TOKEN && ASANA_API_VERSION && ASANA_PROJECT_ID)) {
+  console.log(`${chalk.red('error')}:   The .env file incomplete.  Make sure it includes ASANA_ACCESS_TOKEN, ASANA_API_VERSION and ASANA_PROJECT_ID.`);
+  return;
+}
+const ASANA_API_URL = `https://app.asana.com/api/${ASANA_API_VERSION}`;
+const ASANA_PROJECT_URL=`https://app.asana.com/0/${ASANA_PROJECT_ID}`;
 
-// function to convert the csv to md
-const release = (version) => {
-  const items = [];
-  csv().fromFile(`./releases/v${version.replace(/\./g, '')}.csv`)
-    .on('json', ({ 'Task ID': taskId, Name: name }) => {
-      items.push({ taskId, name });
-    })
-    .on('done', (err) => {
-      if (err) {
-        if (err.message === 'File not exists') {
-          console.log(`${chalk.red('error')}:   CSV export for version ${version} not found.`);
-          return;
+// consistently format error messages
+const logError = msg => console.log(`${chalk.red('error')}:   ${msg}`);
+
+// gets the Asana tagId for a given version tag
+const getTagId = async (tag) => {
+  let tagId;
+  console.log(chalk.yellow('Resolving version tag id...'));
+
+  try {
+    const headers = new fetch.Headers({ Authorization: `Bearer ${ASANA_ACCESS_TOKEN}` });
+    const response = await fetch(`${ASANA_API_URL}/tags`, { headers });
+    if (response.ok) {
+      const { data } = await response.json();
+      data.forEach(({ id, name }) => {
+        if (name === tag) {
+          tagId = id;
         }
-        throw err;
-      }
+      });
+    }
+  } catch (err) {
+    logError('An error occurred while resolving the version tag id.');
+  }
 
-      // generate the release document
-      const notes =
-      `# Version ${version} Release Notes
-      Tasks may be viewed directly on Asana by clicking on their taskId
-      &nbsp;
-      ##### Items completed:
-      ${items.sort(({ taskId: a }, { taskId: b }) => a > b ? 1 : (a < b ? -1 : 0))
-        .map(({ taskId, name }) => (
-          `* [\`${taskId}\`](${ASANA_BASE_URL}/${taskId}) - ${name}`
-        )).join('\n')}
-      `.replace(/ {2,}/g, ''); // replace groups of 2 or more spaces with an empty string for proper formatting
-
-      // write the file out to the releases directory
-      fs.writeFile(`./releases/v${version}.md`, notes, (err) => {
-        if (err) throw err;
-      })
-    });
+  if (!tagId) {
+    logError(`The tag ${tag} was not found.`);
+  }
+  return tagId;
 };
 
+// gets all tasks that have a given tagId
+const getTasks = async (tagId) => {
+  let data;
+  console.log(chalk.yellow('Searching for tasks...'));
+
+  try {
+    const headers = new fetch.Headers({ Authorization: `Bearer ${ASANA_ACCESS_TOKEN}` });
+    // additional fields can be returned with the opt_fields parameter
+    const response = await fetch(`${ASANA_API_URL}/tasks?tag=${tagId}&opt_fields=id,name,projects`, { headers });
+    if (response.ok) {
+      ({ data } = await response.json());
+    }
+  } catch (err) {
+    logError('An error occurred while searching for tasks.');
+  }
+  // filter out any tasks that don't belong to the defined project
+  return data.filter(o => !!o.projects.find(oo => `${oo.id}` === ASANA_PROJECT_ID));
+};
+
+// generates release notes from the Asana tasks
+const genNotes = (version, tasks = []) => {
+  const notes =
+  `# Version ${version} Release Notes
+  Tasks may be viewed directly on Asana by clicking on their taskId
+  &nbsp;
+  ##### Items completed:
+  ${tasks.sort(({ id: a }, { id: b }) => a > b ? 1 : (a < b ? -1 : 0))
+    .map(({ id, name }) => (
+      `* [\`${id}\`](${ASANA_PROJECT_URL}/${id}) - ${name}`
+    )).join('\n')}
+  `.replace(/ {2,}/g, ''); // replace groups of 2 or more spaces with an empty string for proper formatting
+
+  // write the file out to the releases directory
+  fs.writeFile(`./releases/v${version}.md`, notes, (err) => {
+    if (err) throw err;
+  });
+}
+
 // prompt the user for a version string
-const version = process.argv[2];
-if (version) {
-  prompt.override = { version };
+const argVersion = process.argv[2];
+if (argVersion) {
+  prompt.override = { version: argVersion };
 }
 
 prompt.message = '';
@@ -59,8 +98,14 @@ prompt.get([{
   pattern: /^\d+\.\d+\.\d+$/,
   message: 'The version must be in the format x.x.x',
   required: true,
-}], (err, result) => {
-  if (result && result.version) {
-    release(result.version);
+}], async (err, result) => {
+  if (result) {
+    const { version } = result;
+    const tagId = await getTagId(`v${version}`);
+    if (tagId) {
+      const tasks = await getTasks(tagId);
+      genNotes(version, tasks);
+      console.log(chalk.green(`Release notes written to ./releases/v${version}.md`));
+    }
   }
 });
