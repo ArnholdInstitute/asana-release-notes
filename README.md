ab4b648a1919ceb122acea14bcfec625b9de8e19
Kyle <kyle.landell@gmail.com>
2018-01-23 14:29:35 -0500

    Add arg for version and make each item a direct link to Asana

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # Asana Release notes generator
-Generates release notes from a csv export of Asana tasks. Copy the csv export to the releases directory then run the command `yarn release`. The release notes will be copied to the releases directory as `{version#}.md`.
+Generates release notes from a csv export of Asana tasks. Copy the csv export to the releases directory then run the command `yarn release` or `yarn release {version#}`. The release notes will be copied to the releases directory as `{version#}.md`.
--- index.js
+++ index.js
@@ -1,36 +1,37 @@
 const prompt = require('prompt');
 const csv = require('csvtojson');
+const chalk = require('chalk');
 const fs = require('fs');
 
-prompt.start();
+const ASANA_BASE_URL = 'https://app.asana.com/0/430541393561890';
+const VALIDATOR = /^\d+\.\d+\.\d+$/;
+const VALIDATION_MSG = 'The version must be in the format x.x.x';
 
-prompt.get([{
-  name: 'version',
-  type: 'string',
-  description: 'Enter the version number (ex. 1.0.0)',
-  pattern: /^\d+\.\d+\.\d+$/,
-  message: 'The version must be in the format x.x.x',
-  required: true,
-}], (err, { version }) => {
-  if (err) throw err;
-
+// function to convert the csv to md
+const release = (version) => {
   const items = [];
   csv().fromFile(`./releases/v${version.replace(/\./g, '')}.csv`)
     .on('json', ({ 'Task ID': taskId, Name: name }) => {
       items.push({ taskId, name });
     })
     .on('done', (err) => {
-      if (err) throw err;
+      if (err) {
+        if (err.message === 'File not exists') {
+          console.log(`${chalk.red('error')}:   CSV export for version ${version} not found.`);
+          return;
+        }
+        throw err;
+      }
 
       // generate the release document
       const notes =
       `# Version ${version} Release Notes
-      Tasks may be viewed directly on Asana via \`https://app.asana.com/0/430541393561890/{itemID}\`
+      Tasks may be viewed directly on Asana by clicking on their taskId
       &nbsp;
       ##### Items completed:
       ${items.sort(({ taskId: a }, { taskId: b }) => a > b ? 1 : (a < b ? -1 : 0))
         .map(({ taskId, name }) => (
-          `* \`${taskId}\` - ${name}`
+          `* [\`${taskId}\`](${ASANA_BASE_URL}/${taskId}) - ${name}`
         )).join('\n')}
       `.replace(/ {2,}/g, ''); // replace groups of 2 or more spaces with an empty string for proper formatting
 
@@ -39,4 +40,27 @@ prompt.get([{
         if (err) throw err;
       })
     });
+};
+
+// prompt the user for a version string
+const version = process.argv[2];
+if (version) {
+  prompt.override = { version };
+}
+
+prompt.message = '';
+prompt.delimiter = '';
+
+prompt.start();
+prompt.get([{
+  name: 'version',
+  type: 'string',
+  description: chalk.green('Enter the version number (ex. 1.0.0):'),
+  pattern: /^\d+\.\d+\.\d+$/,
+  message: 'The version must be in the format x.x.x',
+  required: true,
+}], (err, result) => {
+  if (result && result.version) {
+    release(result.version);
+  }
 });
